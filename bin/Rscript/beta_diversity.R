5f464266b9b358c7c3f849e44bbdf1a4259430be
Mattia Pandolfo <mattia.pandolfo@gmail.com>
2022-06-01 17:49:14 +0200

    remove source in beta_diversity.R

--- bin/Rscript/beta_diversity.R
+++ bin/Rscript/beta_diversity.R
@@ -4,7 +4,6 @@ shhh(library(data.table))
 shhh(library(plotly))
 shhh(library(phyloseq))
 shhh(library(metagenomeSeq))
-shhh(source("bin/Rscript/filter&CSSnormalize.R"))
 
 # Reading input
 args <- commandArgs(trailingOnly = TRUE)
@@ -22,12 +21,32 @@ metadata$Sample <- rownames(metadata)
 beta_var <- args[4]
 
 # Phyloseq object creation
-ps0 <- phyloseq(otu_table(count, taxa_are_rows = TRUE),
+ps <- phyloseq(otu_table(count, taxa_are_rows = TRUE),
                 tax_table(as.matrix(taxo)),
                 sample_data(metadata))
 
 ####################### FILTER & CSS NORMALIZE #################################
-ps <- filter_CSSnormalize(ps0)
+################################## FILTERING ###################################
+# Filter uncharacterized taxas
+ps_filter <- subset_taxa(ps, !is.na(Phylum) & !Phylum %in% c("", "Unclassified"))
+# relative abundance filter
+FSr  = transform_sample_counts(ps_filter, function(x) x / sum(x))
+FSfr = filter_taxa(FSr, function(x) mean(x) > 0.0001, TRUE)
+keeptaxa = taxa_names(FSfr)
+alltaxa = taxa_names(ps_filter)
+myTaxa = alltaxa[alltaxa %in% keeptaxa]
+physeqaFS <- prune_taxa(myTaxa, ps_filter)
+ps_filter = filter_taxa(physeqaFS, function(x) sum(x >= 1) >= (2), TRUE)
+
+################################ NORMALIZATION #################################
+# CSS
+ps_m <- phyloseq::phyloseq_to_metagenomeSeq(ps_filter)
+# normalized count matrix
+ps_norm <- metagenomeSeq::MRcounts(ps_m, norm = TRUE, log = TRUE)
+# abundance to css-normalized
+phyloseq::otu_table(ps_filter) <- phyloseq::otu_table(ps_norm, taxa_are_rows = T)
+# Restore sample_data rownames
+row.names(ps_filter@sam_data) <- c(1:nrow(ps_filter@sam_data))
 
 ################################# 2D PLOTS #####################################
 # Beta-Diversity measurments
